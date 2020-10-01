be4ae37f044fddac80d18defe0c04af6234fdbcc
Paul Deng <dengpeng.cn@gmail.com>
2020-10-01 21:56:09 +1000

    change to use arduino standard ethernet library

---
+++ src/main.cpp
@@ -0,0 +1,126 @@
+
+/*
+  Initialize code for Leonardo ETH, Ethernet module W5500
+ */
+#include <Arduino.h>
+#include <SPI.h>
+#include <Ethernet.h>
+#include "utility/w5100.h"
+
+#define SS 10  //W5500 CS
+#define RST 11 //W5500 RST
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+#if defined(WIZ550io_WITH_MACADDRESS) // Use assigned MAC address of WIZ550io
+;
+#else
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+#endif
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+EthernetClient client;
+
+boolean state = true;
+void setup()
+{
+  pinMode(SS, OUTPUT);
+  pinMode(RST, OUTPUT);
+
+  digitalWrite(SS, LOW);
+
+  /* If you want to control Reset function of W5500 Ethernet controller */
+  digitalWrite(RST, HIGH);
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  // this check is only needed on the Leonardo:
+  while (!Serial)
+  {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  Serial.println("Start!");
+
+// start the Ethernet connection:
+#if defined(WIZ550io_WITH_MACADDRESS) // Use assigned MAC address of WIZ550io
+  if (Ethernet.begin() == 0)
+  {
+#else
+
+  delay(1000);
+
+  if (Ethernet.begin(mac) == 0)
+  {
+#endif
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    for (;;)
+      ;
+  }
+
+  // print your local IP address:
+  Serial.print("My IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++)
+  {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  digitalWrite(SS, HIGH);
+
+  delay(10000);
+}
+void loop()
+{
+  Serial.println("Power down the W5500");
+
+  W5100.writePHYCFGR_W5500(0b11110111);
+  W5100.writePHYCFGR_W5500(0b01110111);
+
+  delay(10000);
+
+  Serial.println("Power back on the W5500 with '10BT Half-duplex, Auto-negotiation disabled'");
+
+  //set to 10Mbit ethernet
+  W5100.writePHYCFGR_W5500(0b11000111);
+  W5100.writePHYCFGR_W5500(0b01000111);
+  W5100.writePHYCFGR_W5500(0b11000000);
+
+  // softreset
+  uint16_t count = 0;
+  // write to reset bit
+  W5100.writeMR(0x80);
+  // then wait for soft reset to complete
+  do
+  {
+    uint8_t mr = W5100.readMR();
+    if (mr == 0)
+      break;
+    delay(1);
+  } while (++count < 20);
+
+  if (Ethernet.begin(mac) == 0)
+  {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+  }
+
+  // print your local IP address:
+  Serial.print("My IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++)
+  {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print(".");
+  }
+  Serial.println();
+  digitalWrite(SS, HIGH);
+
+  Serial.println();
+
+  delay(10000);
+}
